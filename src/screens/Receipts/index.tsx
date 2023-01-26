196f71e0d273ad533d1ed0f6622ea5b16edd49a6
Pedro Zago <pedro.camargozago@hotmail.com.br>
2023-01-26 17:52:26 -0300

    Storage

--- src/screens/Receipts/index.tsx
+++ src/screens/Receipts/index.tsx
@@ -1,37 +1,82 @@
-import React from 'react';
-import { FlatList } from 'react-native';
+import moment from "moment";
+import React, { useState, useEffect } from "react";
+import { Alert, FlatList } from "react-native";
+import storage from "@react-native-firebase/storage";
 
-import { Container, PhotoInfo } from './styles';
-import { Header } from '../../components/Header';
-import { Photo } from '../../components/Photo';
-import { File } from '../../components/File';
-
-import { photosData } from '../../utils/photo.data';
+import { Container, PhotoInfo } from "./styles";
+import { Header } from "../../components/Header";
+import { Photo } from "../../components/Photo";
+import { File, FileProps } from "../../components/File";
 
 export function Receipts() {
+  moment.locale("pt-br");
+
+  const [photos, setPhotos] = useState<FileProps[]>([]);
+  const [photoSelected, setPhotoSelected] = useState("");
+  const [photoInfo, setPhotoInfo] = useState("");
+
+  async function fetchPhotos() {
+    storage()
+      .ref("/images")
+      .list()
+      .then((result) => {
+        const files: FileProps[] = [];
+
+        result.items.forEach((file) => {
+          files.push({
+            name: file.name,
+            path: file.fullPath,
+          });
+        });
+
+        setPhotos(files);
+      });
+  }
+
+  async function handleShowImage(path: string) {
+    const urlImage = await storage().ref(path).getDownloadURL();
+    setPhotoSelected(urlImage);
+
+    const info = await storage().ref(path).getMetadata();
+    setPhotoInfo(`Upload realizado em ${moment(info.timeCreated).format("l")}`);
+  }
+
+  async function handleDeleteImage(path: string) {
+    storage()
+      .ref(path)
+      .delete()
+      .then(() => {
+        Alert.alert("Deleting image successfully");
+        fetchPhotos();
+      })
+      .catch((error) => console.error(error));
+  }
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
+
   return (
     <Container>
       <Header title="Comprovantes" />
 
-      <Photo uri="" />
+      <Photo uri={photoSelected} />
 
-      <PhotoInfo>
-        Informações da foto
-      </PhotoInfo>
+      <PhotoInfo>{photoInfo}</PhotoInfo>
 
       <FlatList
-        data={photosData}
-        keyExtractor={item => item.name}
+        data={photos}
+        keyExtractor={(item) => item.name}
         renderItem={({ item }) => (
           <File
             data={item}
-            onShow={() => { }}
-            onDelete={() => { }}
+            onShow={() => handleShowImage(item.path)}
+            onDelete={() => handleDeleteImage(item.path)}
           />
         )}
         contentContainerStyle={{ paddingBottom: 100 }}
         showsVerticalScrollIndicator={false}
-        style={{ width: '100%', padding: 24 }}
+        style={{ width: "100%", padding: 24 }}
       />
     </Container>
   );
--- src/screens/Upload/index.tsx
+++ src/screens/Upload/index.tsx
@@ -1,19 +1,24 @@
-import React, { useState } from 'react';
-import * as ImagePicker from 'expo-image-picker';
+import React, { useState } from "react";
+import * as ImagePicker from "expo-image-picker";
 
-import { Button } from '../../components/Button';
-import { Header } from '../../components/Header';
-import { Photo } from '../../components/Photo';
+import storage from "@react-native-firebase/storage";
 
-import { Container, Content, Progress, Transferred } from './styles';
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import { Photo } from "../../components/Photo";
+
+import { Container, Content, Progress, Transferred } from "./styles";
+import { Alert } from "react-native";
 
 export function Upload() {
-  const [image, setImage] = useState('');
+  const [image, setImage] = useState("");
+  const [bytesTransferred, setBytesTransferred] = useState("");
+  const [progress, setProgress] = useState(0);
 
   async function handlePickImage() {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
-    if (status == 'granted') {
+    if (status == "granted") {
       const result = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.Images,
         aspect: [4, 4],
@@ -24,26 +29,54 @@ export function Upload() {
         setImage(result.uri);
       }
     }
-  };
+  }
+
+  async function handleUpload() {
+    const fileName = new Date().getTime();
+
+    const MIME = image.match(/\.(?:.(?!\.))+$/);
+
+    const reference = storage().ref(`/images/${fileName}${MIME}`);
+
+    // reference
+    //   .putFile(image)
+    //   .then(() => Alert.alert("Upload success"))
+    //   .catch((error) => console.log(error));
+
+    const uploadTask = reference.putFile(image);
+
+    uploadTask.on("state_changed", (taskSnapshot) => {
+      const percentage = (
+        (taskSnapshot.bytesTransferred / taskSnapshot.totalBytes) *
+        100
+      ).toFixed(0);
+      setProgress(Number(percentage));
+
+      setBytesTransferred(
+        `${taskSnapshot.bytesTransferred} de ${taskSnapshot.totalBytes}`
+      );
+    });
+
+    uploadTask.then(async () => {
+      const imageUrl = reference.getDownloadURL();
+      console.log(imageUrl);
+      Alert.alert("Upload success");
+    });
+  }
 
   return (
     <Container>
-      <Header title="Lista de compras" />
+      <Header title="Upload de comprovantes" />
 
       <Content>
         <Photo uri={image} onPress={handlePickImage} />
 
-        <Button
-          title="Fazer upload"
-          onPress={() => { }}
-        />
+        <Button title="Fazer upload" onPress={handleUpload} />
 
-        <Progress>
-          0%
-        </Progress>
+        <Progress>{progress}%</Progress>
 
         <Transferred>
-          0 de 100 bytes transferido
+          {bytesTransferred ? bytesTransferred : 0} bytes transferido
         </Transferred>
       </Content>
     </Container>
