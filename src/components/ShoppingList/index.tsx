6bcbe3cbbb3eb60b421d0e476a51441509a6c95e
Pedro Zago <pedro.camargozago@hotmail.com.br>
2023-01-25 23:34:58 -0300

    Cloud Firestore

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -1,18 +1,52 @@
-import React, { useState } from 'react';
-import { FlatList } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { FlatList } from "react-native";
+import firestore from "@react-native-firebase/firestore";
 
-import { styles } from './styles';
-import { Product, ProductProps } from '../Product';
-
-import { shoppingListExample } from '../../utils/shopping.list.data';
+import { styles } from "./styles";
+import { Product, ProductProps } from "../Product";
 
 export function ShoppingList() {
-  const [products, setProducts] = useState<ProductProps[]>(shoppingListExample);
+  const [products, setProducts] = useState<ProductProps[]>([]);
+
+  useEffect(() => {
+    // Pesquisa por todos os registros sem realtime.
+    // firestore()
+    //   .collection("products")
+    //   .get()
+    //   .then((response) => {
+    //     const data = response.docs.map((doc) => {
+    //       return {
+    //         id: doc.id,
+    //         ...doc.data(),
+    //       };
+    //     }) as ProductProps[];
+    //     setProducts(data);
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
+
+    // Pesquisa por todos os registros em realtime.
+    const subscribe = firestore()
+      .collection("products")
+      .onSnapshot((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        }) as ProductProps[];
+
+        setProducts(data);
+      });
+
+    return () => subscribe();
+  }, []);
 
   return (
     <FlatList
       data={products}
-      keyExtractor={item => item.id}
+      keyExtractor={(item) => item.id}
       renderItem={({ item }) => <Product data={item} />}
       showsVerticalScrollIndicator={false}
       style={styles.list}
